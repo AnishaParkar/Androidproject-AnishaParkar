6b9d874ea3c5dc20420d2479bddea606b852e1da
AnishaParkar <anishaparkar95@gmail.com>
2019-12-11 16:27:45 -0500

    Menu displayed with recycler view

--- app/src/main/java/edu/newhaven/android/mytableapp/Adapter/MenuAdapter.kt
+++ app/src/main/java/edu/newhaven/android/mytableapp/Adapter/MenuAdapter.kt
@@ -1,38 +1,79 @@
 package edu.newhaven.android.mytableapp.Adapter
 
+
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import edu.newhaven.android.mytableappv1.MenuModel
+import com.google.firebase.database.FirebaseDatabase
+import edu.newhaven.android.mytableappv1.Menu
 import edu.newhaven.android.mytableappv1.R
 
-class MenuAdapter(val mContext: Context,val layoutResId:Int, val MenuList:List<MenuModel>)
-    :ArrayAdapter<MenuModel>(mContext,layoutResId,MenuList)
-{
-
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-
-        val layoutInflater:LayoutInflater = LayoutInflater.from(mContext);
-        val view: View = layoutInflater.inflate(layoutResId,null)
-
-        val menu_description = view.findViewById<TextView>(R.id.menu_description)
-        val menu_calories = view.findViewById<TextView>(R.id.menu_calories)
-        val menu_cost = view.findViewById<TextView>(R.id.menu_cost)
-        val menu_itemName = view.findViewById<TextView>(R.id.menu_itemName)
+class MenuAdapter(private val menulist: MutableList<MenuModel>, private val context: Context):RecyclerView.Adapter<MenuAdapter.ViewHolder>() {
 
 
-        val menu = MenuList[position]
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.menu_row, parent, false)
 
-        menu_itemName.text = menu.ItemName
-        menu_description.text = menu.Description
-        menu_calories.text = menu.Calories
-        menu_cost.text = menu.Cost
+        return ViewHolder(view)
+    }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val menu= menulist[position]
 
-        return view
+        holder.menu_itemName.text = menu.ItemName
+        holder.menu_calories.text = menu.Calories
+        holder.menu_cost.text = menu.Cost
+        holder.menu_description.text = menu.Description
+
+        holder.add_menu.setOnClickListener { addtoCart(menu) }
 
     }
+
+    override fun getItemCount(): Int {
+        return menulist.size;
+    }
+
+    inner class ViewHolder internal constructor(view: View) : RecyclerView.ViewHolder(view) {
+
+        internal var menu_itemName: TextView
+        internal var menu_cost: TextView
+        internal var menu_calories: TextView
+        internal var menu_description: TextView
+        internal var add_menu:Button
+
+        init {
+            menu_itemName = view.findViewById(R.id.menu_itemName)
+            menu_cost = view.findViewById(R.id.menu_cost)
+            menu_calories = view.findViewById(R.id.menu_calories)
+            menu_description = view.findViewById(R.id.menu_description)
+            add_menu = view.findViewById(R.id.Add_menu)
+        }
+    }
+
+    private fun addtoCart(menu:MenuModel){
+
+//        val db = GenericMethods.database.getReference("Orders")
+//        db.addListenerForSingleValueEvent(object: ValueEventListener {
+//            override fun onCancelled(p0: DatabaseError) {
+//                GenericMethods.pd.dismiss()
+//            }
+//
+//            override fun onDataChange(dataSnapshot: DataSnapshot) {
+//                val pos = dataSnapshot.children.count()+1
+//                info(pos)
+//                val map = HashMap<String, String>()
+//                map["Itemname"] = menu.ItemName
+//                map["cost"] = menu.Cost
+//                db.child("Orders$pos").push()
+//                db.child("Orders$pos").setValue(map)
+//            }
+//        })
+
+    }
+
 }
--- app/src/main/java/edu/newhaven/android/mytableapp/Menu.kt
+++ app/src/main/java/edu/newhaven/android/mytableapp/Menu.kt
@@ -1,7 +1,9 @@
 package edu.newhaven.android.mytableappv1
 
 import android.os.Bundle
-import android.widget.ListView
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -13,15 +15,18 @@ import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class Menu : GenericMethods() {
 
-    lateinit var menuList: MutableList<MenuModel>
-    lateinit var listView: ListView
+    private var menuAdapt: MenuAdapter? = null
+    private lateinit var recyclerView: RecyclerView
+    var menulist = mutableListOf<MenuModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(this@Menu, R.layout.activity_menu)
+        setContentView(R.layout.activity_menu)
 
-        menuList = mutableListOf()
-        listView = findViewById(R.id.menulistview)
+        val mLayoutManager = LinearLayoutManager(applicationContext!!)
+        val tenderListRV = findViewById<View>(R.id.menu_rv) as RecyclerView
+        tenderListRV.layoutManager = mLayoutManager
+
 
         val ref = database.reference.child("Restaurant").child("Olive Garden").child("Menu")
 
@@ -31,55 +36,20 @@ class Menu : GenericMethods() {
             }
 
             override fun onDataChange(p0: DataSnapshot) {
+                menulist = mutableListOf<MenuModel>()
 
                 for(data in p0.children) {
 
                     val item = p0.children.elementAt(0)
                     val menu = data.getValue(MenuModel::class.java)
-                    menuList.add(menu!!)
-
-
+                    menulist.add(menu!!)
                 }
 
-                val adapter = MenuAdapter(applicationContext,R.layout.menu_row,menuList)
 
-                listView.adapter = adapter
-
-//                // Item 1
-//                val item1 = p0.children.elementAt(0)
-//
-//                menu_itemName1.text = item1.key
-//                menu_description1.text = item1.child("Description").value.toString()
-//                menu_cost1.text = item1.child("Cost").value.toString()
-//                menu_calories1.text = item1.child("Calories").value.toString()
-//
-//                // Item 2
-//                val item2 = p0.children.elementAt(1)
-//
-//                menu_itemName2.text = item2.key
-//                menu_description2.text = item2.child("Description").value.toString()
-//                menu_cost2.text = item2.child("Cost").value.toString()
-//                menu_calories2.text = item2.child("Calories").value.toString()
-//
-//                // Item 3
-//                val item3 = p0.children.elementAt(2)
-//
-//                menu_itemName3.text = item3.key
-//                menu_description3.text = item3.child("Description").value.toString()
-//                menu_cost3.text = item3.child("Cost").value.toString()
-//                menu_calories3.text = item3.child("Calories").value.toString()
-//
-//                // Item 4
-//                val item4 = p0.children.elementAt(3)
-//
-//                menu_itemName4.text = item4.key
-//                menu_description4.text = item4.child("Description").value.toString()
-//                menu_cost4.text = item4.child("Cost").value.toString()
-//                menu_calories4.text = item4.child("Calories").value.toString()
+                val adapter = MenuAdapter(menulist,applicationContext)
+                tenderListRV.adapter = adapter
             }
         })
-
-
     }
 }
 
